e8d10165aa6063255e5e2083456ba21e689d39b6
Kenjicci <Cincokencstd1@gmail.com>
2024-12-22 20:48:58 +0800

    created class for pendulum attributes and functions

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Pendulum Simulation  
-
+  
+## Description
 <p>This simulation reflects the formula of the pendulum: </p>
 <br>
+<p>
+
+## Dependencies  
+
+
+## How to run it?
--- pendulum.py
+++ pendulum.py
@@ -1,6 +1,26 @@
-#libraries and modules
 import numpy as np
-import matplotlib as plot
-import pygame as ps
+import sympy as sp
+from sympy.utilities import lambdify
+from sympy.solvers import solve
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from matplotlib import animation
+from matplotlib.animation import PillowWriter
+from IPython.display import HTML
+plt.style.use('seaborn-v0_8')
 
-#formula
+class Pendulum:
+    def __init__(self, l, t, theta0, v0):
+        self.l = l
+        self.t = t
+        self.theta0 = theta0
+        self.conditions = [theta0, v0]
+
+    def solve_pendulum_motion(self):
+        m, g, l, t = sp.symbols(('m', 'g', 'l', 't'))
+        theta = sp.Function('theta')(t)
+        print(theta)
+    
+t = np.linspace(0, 10, 500)
+pend = Pendulum(1, t, np.pi/4, 0)
+pend.solve_pendulum_motion()
